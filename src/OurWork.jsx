85d63ba78d1edd77a0dc5905ca069e92a7d1a8ef
Erich Gaul <erichgaul@gmail.com>
2023-10-14 20:34:32 -0500

    Recovering repo

--- src/OurWork.jsx
+++ src/OurWork.jsx
@@ -6,6 +6,7 @@ import community from "./assets/icons/community.png"
 import greenspace from "./assets/icons/greenspace.png"
 import repair from "./assets/icons/repair.png"
 import smallbusiness from "./assets/icons/smallbusiness.png"
+import gardenwoman from "./assets/ourworkphotos/greenspace/gardeningwoman.png"
 import { useReducer } from "react";
 
 
@@ -19,33 +20,42 @@ function OurWork() {
                 <h1 className="outline-text">OUR WORK</h1>
             </div>
 
-            <div className="services-picker">
-                <div className="service" id="greenspace">
-                    <img src={greenspace} alt="Icon of hand holding plant" />
-                    <p className="service-subtitle">GREEN SPACE</p>
+            <section className="our-work">
+                <div className="our-work-left">
+                    <div className="services-picker">
+                        <div className="service" id="greenspace">
+                            <img src={greenspace} alt="Icon of hand holding plant" />
+                            <p className="service-subtitle">GREEN SPACE</p>
+                        </div>
+                        <div className="service" id="cleanup">
+                            <img src={cleanup} alt="Icon of a trash can" />
+                            <p className="service-subtitle">CLEANUP</p>
+                        </div>
+                        <div className="service" id="smallbusiness">
+                            <img src={smallbusiness} alt="Icon of two hands shaking" />
+                            <p className="service-subtitle">SMALL BUSINESS</p>
+                        </div>
+                        <div className="service" id="community">
+                            <img src={community} alt="Icon of a group of people" />
+                            <p className="service-subtitle">COMMUNITY</p>
+                        </div>
+                        <div className="service" id="architecture">
+                            <img src={architecture} alt="Icon of architecture tools" />
+                            <p className="service-subtitle">ARCHITECTURE & DESIGN</p>
+                        </div>
+                        <div className="service" id="repair">
+                            <img src={repair} alt="Icon of a hammer and saw" />
+                            <p className="service-subtitle">REPAIR/BUILD</p>
+                        </div>
+                    </div>
+                    <div className="our-work-blurb">
+                        <p>at urban rebirth, we create vibrant urban green spaces, transforming neighborhoods into lush oases. Our services include designing and maintaining parks and promoting sustainable gardening in the heart of the city. partner with us to build a greener, healthier, and more sustainable urban landscape for all.</p>
+                    </div>
                 </div>
-                <div className="service" id="cleanup">
-                    <img src={cleanup} alt="Icon of a trash can" />
-                    <p className="service-subtitle">CLEANUP</p>
+                <div className="photo-switcher our-work-right">
+                    <img src={gardenwoman} alt="Woman gardening" />
                 </div>
-                <div className="service" id="smallbusiness">
-                    <img src={smallbusiness} alt="Icon of two hands shaking" />
-                    <p className="service-subtitle">SMALL BUSINESS</p>
-                </div>
-                <div className="service" id="community">
-                    <img src={community} alt="Icon of a group of people" />
-                    <p className="service-subtitle">COMMUNITY</p>
-                </div>
-                <div className="service" id="architecture">
-                    <img src={architecture} alt="Icon of architecture tools" />
-                    <p className="service-subtitle">ARCHITECTURE & DESIGN</p>
-                </div>
-                <div className="service" id="repair">
-                    <img src={repair} alt="Icon of a hammer and saw" />
-                    <p className="service-subtitle">REPAIR/BUILD</p>
-                </div>
-            </div>
-
+            </section>
             <div className="horizontal-bar"></div>
             <Footer />
         </>
