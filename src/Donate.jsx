7a9cc080cb3c5222c8ef48edd151b80cf6061412
Erich Gaul <erichgaul@gmail.com>
2023-10-19 16:46:25 -0500

    Finish layout of donate amount picker

--- src/Donate.jsx
+++ src/Donate.jsx
@@ -1,4 +1,5 @@
 import './styles/donate.css'
+import donate_logo from './assets/donate/donate_logo.png'
 import Header from './Header.jsx'
 import { ArrowRight } from 'lucide-react'
 import { useState } from 'react'
@@ -22,6 +23,40 @@ function Donate() {
             <h2 className='card-subtitle'>
                 every penny counts. we appreciate your generosity!
             </h2>
+            <div className='card-slider'>
+                <div className='slider-choice-background'></div>
+                <p className='slider-option'>one-time</p>
+                <p className='slider-option'>monthly</p>
+            </div>
+            <form className='amount-options-container flex'>
+                <div className="donate-option flex">
+                    <input name='donate-option' type="radio" />
+                    <h3 className='donate-option-amt'><sup>$</sup>15</h3>
+                    <p className="donate-option-desc">one gallon of tempura paint for crosswalks and street murals</p>
+                </div>
+                <div className="donate-option flex">
+                    <input name='donate-option' type="radio" />
+                    <h3 className='donate-option-amt'><sup>$</sup>50</h3>
+                    <p className="donate-option-desc">One sheet of plywood for outdoor cafe or subflooring</p>
+                </div>
+                <div className="donate-option flex">
+                    <input name='donate-option' type="radio" />
+                    <h3 className='donate-option-amt'><sup>$</sup>500</h3>
+                    <p className="donate-option-desc">For pop-up plaza lighting and greenery</p>
+                </div>
+                <div className="donate-option flex">
+                    <input name='donate-option' type="radio" />
+                    <p className="donate-option-desc">Custom amount</p>
+                </div>
+            </form>
+            <div aria-label='button' className='donate-next-button flex'>
+                <p>NEXT</p>
+                <ArrowRight size={35} color='white'/>
+            </div>
+            <div className='button-subtitle flex'>
+                <p>powered by</p>
+                <img src={donate_logo} alt="Bloomerang company logo" />
+            </div>
 
         </div>
         </>
